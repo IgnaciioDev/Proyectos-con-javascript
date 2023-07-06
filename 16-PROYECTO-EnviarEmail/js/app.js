c6b961a8acfedb4dfba12446ef7c337f1f17eb00
Ignacio <ignacio9781@gmail.com>
2023-07-05 22:12:50 -0400

    add formulario

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -4,13 +4,14 @@ document.addEventListener('DOMContentLoaded', function(){ // Esto se va ejecutar
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
-
+    const formulario = document.querySelector('#formulario');
     // Asignar eventos
     inputEmail.addEventListener('blur', validar); // cuando el evento ocurra (blur) se ejectua la funcion o mejor dicho callback
     
     inputAsunto.addEventListener('blur', validar);
     
     inputMensaje.addEventListener('blur', validar);
+
 
     function validar(e) {
         if(e.target.value.trim() === '') { // trim es para eliminar los espacios en blanco
@@ -22,6 +23,10 @@ document.addEventListener('DOMContentLoaded', function(){ // Esto se va ejecutar
 
 
     function mostrarAlerta() {
+        // Generar aletar en HTML
+        const error = document.createElement('P');
+        error.textContent = 'Hubo un error...'
+
         console.log('Hubo un error...')
     }
 });
