973e93f2a66077e3c0403927c545600492d8f781
Ignacio <ignacio9781@gmail.com>
2023-07-12 12:39:45 -0400

    agregando referencia

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -15,19 +15,19 @@ document.addEventListener('DOMContentLoaded', function(){ // Esto se va ejecutar
 
     function validar(e) { // e es para encontrar informacion del evento
         if(e.target.value.trim() === '') { // trim es para eliminar los espacios en blanco
-            mostrarAlerta(`El campo ${e.target.id} es obligatorio`);
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
         }else {
             console.log('Si hay algo');
         }
     }
 
 
-    function mostrarAlerta(mensaje) {
+    function mostrarAlerta(mensaje, referencia) {
         // Generar aletar en HTML
         const error = document.createElement('P');
         error.textContent = mensaje;
         error.classList.add('bg-red-600', 'text-white', 'p-2','text-center'); // agregano clases 
         // Inyectar el error al formulario
-        formulario.appendChild(error);
+        referencia.appendChild(error);
     }
 });
