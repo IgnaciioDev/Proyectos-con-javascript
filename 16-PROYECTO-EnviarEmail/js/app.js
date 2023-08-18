0c6da5927eb6511733e322db72b172f4a6dab1d2
Ignacio Castro <ignacio9781@gmail.com>
2023-08-18 02:50:36 -0400

    Limpiar alerta

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -16,18 +16,15 @@ document.addEventListener('DOMContentLoaded', function(){ // Esto se va ejecutar
     function validar(e) { // e es para encontrar informacion del evento
         if(e.target.value.trim() === '') { // trim es para eliminar los espacios en blanco
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-        }else {
-            console.log('Si hay algo');
+            return;
         }
+
+        limpiarAlerta(e.target.parentElement);
     }
 
 
     function mostrarAlerta(mensaje, referencia) {
-        // Comprueba si ya existe una alerta
-        const alerta = referencia.querySelector('.bg-red-600');
-        if(alerta) {
-            alerta.remove();
-        }
+        limpiarAlerta(referencia);
 
         // Generar aletar en HTML
         const error = document.createElement('P');
@@ -35,5 +32,13 @@ document.addEventListener('DOMContentLoaded', function(){ // Esto se va ejecutar
         error.classList.add('bg-red-600', 'text-white', 'p-2','text-center'); // agregano clases 
         // Inyectar el error al formulario
         referencia.appendChild(error);
+    }
+
+    function limpiarAlerta(referencia) {
+        // Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta) {
+            alerta.remove();
+        }
     }
 });
