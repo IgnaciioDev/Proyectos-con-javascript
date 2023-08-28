6df2ffcb984bd0e95975c36d8e4c3fe249afcee1
Ignacio Castro <ignacio9781@gmail.com>
2023-08-28 14:19:45 -0400

    como usar y almacenar arreglos y objetos

--- 18-LocalStorage/js/01-app.js
+++ 18-LocalStorage/js/01-app.js
@@ -1,25 +1,36 @@
-// Veamos algunos ejemeplos de LocalStorage...
+// Recuerda que en localStorage solo se puede almacenar string,
+// No se puede guardar arreglos y objetos
 
-// Local Storage funciona con una especie de Llave valor...
+// localStorage.setItem('nombre', 'Ignacio');
+
+// es una llave un valor, la llave es como vamos a tomar esos valores
+// y el valor lo que se puede cambiar
+
+// sessionStorage.setItem('Nombre', 'Juan');
 
 
-localStorage.setItem('nombre', 'Juan');
 
-// añadir algo a sessionstorage
-sessionStorage.setItem('nombre', 'Pablo');
+// Una manera de almacenar un objeto
 
-// Local Storage solo soporta strings, no soporta arrays ni objetos pero puedes almacenarlos convirtiendolos a string..
 
 const producto = {
-    nombre: 'Monitor 24"',
+    nombre: 'Monitor 24 Pulgadas',
     precio: 300
 }
 
 const productoString = JSON.stringify(producto);
-localStorage.setItem('productoJSON', productoString);
+
+console.log(typeof productoString);
 
 
+localStorage.setItem('Nombre', productoString);
 
-// Lo mismo con un array
- const meses = ['Enero', 'Febrero', 'Marzo'];
-localStorage.setItem('meses',  JSON.stringify(meses));
+// Una manera de almacenar un arreglo
+
+const meses = ['Enero', 'Febrero', 'Marzo'];
+
+const mesesString = JSON.stringify(meses);
+
+console.log(typeof mesesString);
+
+localStorage.setItem('meses', mesesString);
