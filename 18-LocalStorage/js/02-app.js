03f804898ce59581bdb1de282986c4806d230249
Ignacio Castro <ignacio9781@gmail.com>
2023-08-28 15:19:46 -0400

    obteniendo datos y validno arrays y objet

--- 18-LocalStorage/js/02-app.js
+++ 18-LocalStorage/js/02-app.js
@@ -1,13 +1,13 @@
-// En este video veamos como obtener lo sdatos de Local storage
-
+// Recuerda que getItem es para obtener el valor
 const nombre = localStorage.getItem('nombre');
 console.log(nombre);
 
 
-const productoJSON = localStorage.getItem('productoJSON');
-console.log(JSON.parse( productoJSON ));
+const productoJSON = localStorage.getItem('producto');
 
+console.log(JSON.parse(productoJSON));
 
-const meses = JSON.parse( localStorage.getItem('meses'));
+const meses = localStorage.getItem('meses');
 console.log(meses);
 
+console.log(JSON.parse(meses));
