64bc106f46452d76867bd123f326815d456bca15
Ignacio Castro <ignacio9781@gmail.com>
2023-08-28 16:17:40 -0400

    valindando formulario y mostrando error

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -1,115 +1,47 @@
 // Variables
-const listaTweets = document.querySelector('#lista-tweets');
 const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
 let tweets = [];
 
+
 // Event Listeners
-eventListeners();
+eventListener();
 
-function eventListeners() {
-     //Cuando se envia el formulario
+function eventListener() {
      formulario.addEventListener('submit', agregarTweet);
-
-     // Borrar Tweets
-     listaTweets.addEventListener('click', borrarTweet);
-
-     // Contenido cargado
-     document.addEventListener('DOMContentLoaded', () => {
-          tweets = JSON.parse( localStorage.getItem('tweets') ) || []  ;
-          console.log(tweets);
-          crearHTML();
-     });
 }
 
-// Añadir tweet del formulario
+// Funciones
+
 function agregarTweet(e) {
-     e.preventDefault();
-     // leer el valor del textarea
-     const tweet = document.querySelector('#tweet').value;
+     e.preventDefault() 
      
-     // validación
+     // Textarea donde el usuario escribe
+     const tweet = document.querySelector('#tweet').value;
+
+     // Validacion
      if(tweet === '') {
           mostrarError('Un mensaje no puede ir vacio');
-          return;
+          return; // Evita que se ejecuten mas lineas de codigo esto funciona siempre una funcion
      }
 
-     // Crear un objeto Tweet
-     const tweetObj = {
-          id: Date.now(),
-          texto: tweet
-     }
-
-     // Añadirlo a mis tweets
-     tweets = [...tweets, tweetObj];
-     
-     // Una vez agregado, mandamos renderizar nuestro HTML
-     crearHTML();
-
-     // Reiniciar el formulario
-     formulario.reset();
+     console.log('Agregando tweets');
 }
+
+// Mostrar mensaje de error
 
 function mostrarError(error) {
-     const mensajeEerror = document.createElement('p');
-     mensajeEerror.textContent = error;
-     mensajeEerror.classList.add('error');
+     const mensajeError = document.createElement('p');
+     mensajeError.textContent = error;
+     mensajeError.classList.add('error');
+
+     // Insertarlo en el contenido
 
      const contenido = document.querySelector('#contenido');
-     contenido.appendChild(mensajeEerror);
+     contenido.appendChild(mensajeError);
 
+     // Elimina la alerta despues de 3 segundos
      setTimeout(() => {
-          mensajeEerror.remove();
-     }, 3000);
-}
-
-function crearHTML() {
-     limpiarHTML();
-     
-     if(tweets.length > 0 ) {
-          tweets.forEach( tweet =>  {
-               // crear boton de eliminar
-               const botonBorrar = document.createElement('a');
-               botonBorrar.classList = 'borrar-tweet';
-               botonBorrar.innerText = 'X';
-     
-               // Crear elemento y añadirle el contenido a la lista
-               const li = document.createElement('li');
-
-               // Añade el texto
-               li.innerText = tweet.texto;
-
-               // añade el botón de borrar al tweet
-               li.appendChild(botonBorrar);
-
-               // añade un atributo único...
-               li.dataset.tweetId = tweet.id;
-
-               // añade el tweet a la lista
-               listaTweets.appendChild(li);
-          });
-     }
-
-     sincronizarStorage();
-}
-
-// Elimina el Tweet del DOM
-function borrarTweet(e) {
-     e.preventDefault();
-
-     // console.log(e.target.parentElement.dataset.tweetId);
-     const id = e.target.parentElement.dataset.tweetId;
-     tweets = tweets.filter( tweet => tweet.id != id  );
-     crearHTML();
-}
-
-// Agrega tweet a local storage
-function sincronizarStorage() {
-     localStorage.setItem('tweets', JSON.stringify(tweets));
-}
-
-// Elimina los cursos del carrito en el DOM
-function limpiarHTML() {
-     while(listaTweets.firstChild) {
-          listaTweets.removeChild(listaTweets.firstChild);
-     }
+          mensajeError.remove();
+     }, 3000); // 3s
 }
